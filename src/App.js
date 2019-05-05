cbd7ae7916e0627931219b9abcfec06de00be68d
sonusuccessive <son.kumar@successive.tech>
2019-05-05 21:50:32 +0530

    fist commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Feed from './components/Feed';
+class App extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      contacts: []
+    }
+  }
+  componentDidMount() {
+    fetch("https://api.randomuser.me/?results=50")
+      .then(response => response.json())
+      .then(parsedResponse => {
+        console.log("check response data::::::::", parsedResponse);
+        return parsedResponse.results.map(user => ({
+          name: `${user.name.first} ${user.name.last}`,
+          email: user.email,
+          thumbnail: user.picture.thumbnail
+        }));
+      }
+      )
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      .then(contacts => {
+        console.log("contract:::::::::::", contacts);
+        this.setState({ contacts })
+      });
+    console.log("this.state:::::::::::", this.state)
+  }
+  render() {
+
+
+
+    /*  < div className = "App" >
+         <Feed contacts={this.state.contacts} />
+      //  <h4>{this.state.contacts.map((ele,i) => <p key={i}>{ele}</p>)}</h4>
+     </div > */
+    return (
+      <div className="App">
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                HOC Demo app
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <Feed contacts={this.state.contacts} />
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/components/Feed.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import FeedItem from "./FeedItem";
+import Loading from "./HOC/Loading";
+
+class Feed extends Component {
+  render() {
+    const { loadingTime } = this.props;
+    console.log("loadingTime", loadingTime);
+    return (
+      <div className="feed">
+        <FeedItem contacts={this.props.contacts} />
+        <p>Loading time {loadingTime} seconds</p>
+      </div>
+    );
+  }
+}
+
+export default Loading("contacts")(Feed);
